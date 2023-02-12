157ed5a40ff12673e189f954e86a61cfd82b4aac
Krotenhannes <Tom_b@fietje.de>
2023-02-12 15:48:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from PyQt5 import QtWidgets, QtGui
 import requests
 import configparser
 
+debug = True
+
 class SystemTrayIcon(QtWidgets.QSystemTrayIcon):
     def __init__(self, icon, parent=None):
         QtWidgets.QSystemTrayIcon.__init__(self, icon, parent)
@@ -23,47 +25,103 @@ class SystemTrayIcon(QtWidgets.QSystemTrayIcon):
         
         self.config = configparser.ConfigParser()
         self.config.read('conf.ini')
-        self.wled_ip = f"http://{self.config.get('settigs', 'wled_ip')}"
+
+        self.wled_url = f"http://{self.config.get('settigs', 'wled_ip')}"
+        self.off_preset = f"{self.config.get('settigs', 'off_preset')}"
+        self.on_preset = f"{self.config.get('settigs', 'on_preset')}"
         self.on_power = self.config.getboolean('settigs', 'onPowerWLED')
+
+        self.switch_on_icon = QtGui.QIcon("./src/switch-on.png")
+        self.switch_off_icon = QtGui.QIcon("./src/switch-off.png")
+
+        self.on = False
+
+        response = requests.get(self.wled_url+"/json/state")
+
+        if response.status_code == 200:
+            data = response.json()
+            current_preset = data["ps"]
+        else:
+            if debug:
+                print("Unable to retrieve JSON data, response code:", response.status_code)
+                sys.exit()
+
+        if debug:
+            print(f'WLED URL: {self.wled_url}')
+            print("Current Preset:", current_preset)
+            print(f'Presets: On {self.on_preset}, Off {self.off_preset}')
+
         self.power_on_action.setChecked(self.on_power)
 
         if self.on_power:
-            self.toggel_power_action.setIcon(QtGui.QIcon("./src/switch-on.png"))
-            icon = QtGui.QIcon("./src/switch-on.png")
-            requests.post(f'{self.wled_ip}/json/state', json={"on":True})
+            if current_preset == int(self.off_preset):
+                self.toggel_power_action.setIcon(self.switch_on_icon)
+                requests.post(f'{self.wled_url}/json/state', json={"ps":self.on_preset})
+                if debug:
+                    print("Send a POST request with JSON {'ps': self.on_preset} to self.wled_url/json/state")
+            elif current_preset == int(self.on_preset):
+                self.toggel_power_action.setIcon(self.switch_on_icon)
+            else:
+                self.toggel_power_action.setIcon(self.switch_on_icon)
+                requests.post(f'{self.wled_url}/json/state', json={"ps":self.on_preset})
+                if debug:
+                    print("Send a POST request with JSON {'ps': self.on_preset} to self.wled_url/json/state")
 
     def power_on(self):
         self.config.read('conf.ini')
         self.on_power = not self.on_power
         self.config.set('settigs', 'onPowerWLED', str(self.on_power))
+
         with open('conf.ini', 'w') as configfile:
             self.config.write(configfile)
         self.power_on_action.setChecked(self.on_power)
+
         if self.on_power:
-            self.toggel_power_action.setIcon(QtGui.QIcon("./src/switch-on.png"))
-            print("configured to power on after log in")
-            requests.post(f'{self.wled_ip}/json/state', json={"on":True})
+            self.toggel_power_action.setIcon(self.switch_on_icon)
+            requests.post(f'{self.wled_url}/json/state', json={"ps":self.on_preset})
+            if debug:
+                print("configured to power on after log in")
         else:
-            print("configured to not power on after log in")
+            if debug:
+                print("configured to not power on after log in")
     
     def toggel_power(self):
-        url = f'{self.wled_ip}/json/state'
+        url = f'{self.wled_url}/json/state'
         response = requests.get(url)
-        data = response.json()
-        on = bool(data["on"])
-        if on:
-            self.toggel_power_action.setIcon(QtGui.QIcon("./src/switch-off.png"))
-            requests.post(f'{self.wled_ip}/json/state', json={"on":False})
+        if response.status_code == 200:
+            data = response.json()
+            current_preset = data["ps"]
+            if debug:
+                print(f'Toggel current preset: {data["ps"]}')
         else:
-            self.toggel_power_action.setIcon(QtGui.QIcon("./src/switch-on.png"))
-            requests.post(f'{self.wled_ip}/json/state', json={"on":True})
-        
+            if debug:
+                print("Unable to retrieve JSON data, response code:", response.status_code)
+
+        if current_preset == int(self.off_preset):
+            self.on = False
+        elif current_preset == int(self.on_preset):
+            self.on = True
+        else:
+            self.on = False
+
+        if debug:
+            print(f"ON (TRUE) Turnoff\nSet preset: {self.off_preset}") if self.on else print(f"ON (FALSE) turn on\nSet preset: {self.on_preset}")
+
+        if self.on:
+            self.toggel_power_action.setIcon(self.switch_off_icon)
+            requests.post(f'{self.wled_url}/json/state', json={"ps":self.off_preset})
+            self.on = False
+        else:
+            self.toggel_power_action.setIcon(self.switch_on_icon)
+            requests.post(f'{self.wled_url}/json/state', json={"ps":self.on_preset})
+            self.on = True
 
     def quit(self):
         sys.exit()
-
-app = QtWidgets.QApplication(sys.argv)
-icon = QtGui.QIcon("./src/favicon.ico")
-tray_icon = SystemTrayIcon(icon)
-tray_icon.show()
-sys.exit(app.exec_())
+        
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    icon = QtGui.QIcon("./src/favicon.ico")
+    tray_icon = SystemTrayIcon(icon)
+    tray_icon.show()
+    sys.exit(app.exec_())
